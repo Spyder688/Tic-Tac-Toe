e03088cc5e31b6ce575e322ce43a51ce6c01c742
Spyder688 <krishnakumar191elite@gmail.com>
2025-09-13 16:06:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector(".reset-btn");
+let newgamebtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");  // fixed id
+
+let turnO = true;
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations 🎉 Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableAllBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val !== "" && pos1Val === pos2Val && pos2Val === pos3Val) {
+      showWinner(pos1Val);
+      return; // stop checking after a winner
+    }
+  }
+};
+
+const disableAllBoxes = () => {
+  boxes.forEach((box) => (box.disabled = true));
+};
+
+const enableAllBoxes = () => {
+  boxes.forEach((box) => {
+    box.disabled = false;
+    box.innerText = "";
+  });
+};
+
+resetbtn.addEventListener("click", () => {
+  enableAllBoxes();
+  msgContainer.classList.add("hide");
+  turnO = true;
+});
+
+newgamebtn.addEventListener("click", () => {
+  enableAllBoxes();
+  msgContainer.classList.add("hide");
+  turnO = true;
+});
